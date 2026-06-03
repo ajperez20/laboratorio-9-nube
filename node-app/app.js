1391cdc0200f05df80a92a0fd19b93e1bebd648a
ajperez20 <ajperez.20@est.ucab.edu.ve>
2026-06-02 21:52:02 -0400

    testing connection

--- node-app/app.js
+++ node-app/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World from Docker and Jenkins!');
+  res.send('Hello, World from Docker and Jenkins!!');
 });
 
 app.listen(port, () => {
